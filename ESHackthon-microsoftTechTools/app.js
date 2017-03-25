cae2ad928811742db4c74857990207bebdcacc08
billywithbelly <billychen1216@gmail.com>
2017-03-25 10:39:56 +0800

    initial commit

---
+++ ESHackthon-microsoftTechTools/app.js
@@ -0,0 +1,120 @@
+var express = require('express');
+var request = require("request");
+var btoa = require("btoa");
+var querystring = require('querystring');
+var app = express();
+
+function getRandomArbitrary(min, max) {
+    var number = Math.floor(Math.random() * (max - min + 1)) + min;
+    return number.toString();
+}
+
+app.use(express.static('public'));
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+    res.send('hello world');
+});
+
+app.get('/login', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/branches', function(req, res) {
+  console.log("/branches");
+    var params = {
+        client_id: "3a6c1054-fc3a-48cb-805e-af6e96a49598"
+    }
+    var options = {
+        url: 'https://eospu.esunbank.com.tw/esun/bank/location/branches?',
+        qs: params
+    };
+
+    function callback(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.parse(body));       
+      }
+      else {
+        console.log("[Error][Branches]", error);
+      }
+    }
+
+    request(options, callback);
+});
+
+app.get('/market', function(req, res) {
+    //plan=4&risk=01&invest_time=0&age=30
+    var params = {
+        client_id: "3a6c1054-fc3a-48cb-805e-af6e96a49598",
+        risk: "01",
+        invest_time: "0",
+        age: "30",
+        plan: "4"
+    }
+    var options = {
+        url: 'https://eospu.esunbank.com.tw/esun/bank/information/robot_insight?',
+        qs: params
+    };
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.parse(body));
+        }
+    }
+    request(options, callback);
+});
+
+app.post('/oauth/authorize', function(req, res) {
+
+    var client_id = "3a6c1054-fc3a-48cb-805e-af6e96a49598";
+    var client_secret = "wS8wY5hT2bR7yU0eA3cS0pU6eN0dU4qH8tL2iN3oQ2jE7wV8sB";
+    var username = "B204449696";
+    var password = "265434";
+    var auth_value = "Basic " + btoa(client_id + ":" + client_secret);
+    console.log("[Debug][Authorization][Value]::" + auth_value);
+
+    var params = {
+        grant_type: "password",
+        username: username,
+        password: password,
+        scope: "/user"
+    }
+
+    var formData = querystring.stringify(params);
+    var contentLength = formData.length;
+
+    request({
+        headers: {
+            'Content-Length': contentLength,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization' : auth_value,
+        },
+        uri: 'https://eospu.esunbank.com.tw/esun/bank/user/oauth/authorize',
+        body: formData,
+        method: 'POST'
+    }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("[Debug][API][/oauth/authorize][Response]::" + body);
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.parse(body));
+        } else {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the Google homepage.        
+        }
+    });
+
+});
+
+app.get('/speech', function(req, res) {
+	res.sendFile(__dirname + '/public/eusansystem.html');
+});	
+
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
+});
+
